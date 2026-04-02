dfdb1c530032e6cc86e42570a0293f8d49ed49e1
thefilali <reda@fpgroup.ma>
2026-04-02 19:36:12 +0100

    add components/brand/PoweredBy.tsx

---
+++ components/brand/PoweredBy.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export function PoweredBy() {
+  return (
+    <div className="text-center py-4">
+      <p className="text-text-dim text-xs tracking-wider uppercase">
+        Powered by ISuccess
+      </p>
+    </div>
+  );
+}
